2431b645c8f4504218ded90871dde8961a5b252a
Ryan Demo <ryan.demo@me.com>
2018-02-01 20:24:46 -0500

    Initial Commit

---
+++ StarWarsShipViewerTests/StarWarsShipViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  StarWarsShipViewerTests.swift
+//  StarWarsShipViewerTests
+//
+//  Created by Ryan Demo on 2/1/18.
+//  Copyright © 2018 Ryan Demo. All rights reserved.
+//
+
+import XCTest
+@testable import StarWarsShipViewer
+
+class StarWarsShipViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
